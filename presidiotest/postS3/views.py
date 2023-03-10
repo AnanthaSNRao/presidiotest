0ada919ab40160bc78669791db38aaa32b6c370a
Anantha S N <ananthasn@Ananthas-MacBook-Pro.local>
2023-03-10 15:38:42 -0800

    skeleton template

--- presidiotest/postS3/views.py
+++ presidiotest/postS3/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# def my_view(request):
+#     print('myview-------')
+#     if request.method == 'POST':
+#         my_file = request.FILES['my_file']
+#         file_contents = my_file.read().decode('utf-8')
+#         # Do something with the file contents
+#         return HttpResponse('File uploaded successfully.')
+#     else:
+#         return render(request, 'form.html')
+    
+def get(request):
+    print('myview-------')
+    if request.method == 'POST':
+        my_file = request.FILES['my_file']
+        file_contents = my_file.read().decode('utf-8')
+        # Do something with the file contents
+        return HttpResponse('File uploaded successfully.')
+    else:
+        return render(request, 'form.html')
+
