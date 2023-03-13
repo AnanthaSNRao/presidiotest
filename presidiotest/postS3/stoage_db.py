5f81f3fb4880f5fba3a96d77ebad6de4444977d7
Anantha S N <ananthasn@Ananthas-MacBook-Pro.local>
2023-03-13 10:07:38 -0700

    empty records if needed

--- presidiotest/postS3/stoage_db.py
+++ presidiotest/postS3/stoage_db.py
@@ -5,13 +5,14 @@ from django.core.files.storage import FileSystemStorage
 from decouple import config
 import csv
 import logging
+from datetime import datetime
 
 logger = logging.getLogger("presidiotest")
 S3_client = boto3.client('s3',  aws_access_key_id= config('AWS_ACCESS_KEY_ID'),
          aws_secret_access_key= config('AWS_SECRET_ACCESS_KEY'))
 dynamodb_client = boto3.resource('dynamodb',  aws_access_key_id= config('AWS_ACCESS_KEY_ID'),
          aws_secret_access_key= config('AWS_SECRET_ACCESS_KEY'), region_name= config('AWS_REGION_NAME'))
-tmpfilename = 'lts-Presidio.csv'
+tmpfilename = 'lts-Presidio' + str(datetime.now()) +'.csv' 
 
 
 def handler():
@@ -43,9 +44,11 @@ def handler():
                 break
                 
     # Upload temp file to S3
-    S3_client.upload_file(tmpfilename, config('OUTPUT_BUCKET') ,config('OUTPUT_KEY'))
+    S3_client.upload_file((tmpfilename), config('OUTPUT_BUCKET') ,config('OUTPUT_KEY'))
     location = config('AWS_REGION_NAME')
     os.remove(tmpfilename)
+    if(config('EMPTY_DB_WHILE_BACK_UP')== 'Yes'):
+        empty_dynamodb()
     url = "https://s3-%s.amazonaws.com/%s/%s" % (location, config('OUTPUT_BUCKET'), config('OUTPUT_KEY'))
     return url
    
@@ -62,15 +65,27 @@ def get_list():
 
     return table_names
 
+# def get_S3_object_list(name):
+#     response = S3_client.list_objects_v2(Bucket= name)
+#     objects = response['Contents']
+#     return objects
 
+def empty_dynamodb():
+    table = dynamodb_client.Table(config('TABLE_NAME'))
+    response = table.scan()
 
-def upload(f):
-    if not f:
+    with table.batch_writer() as batch:
+        print(response['Items'])
+        for id in response['Items']['id']:
+            batch.delete_item(Key=id)
+
+def upload(file):
+    if not file:
         return False
-    name = f.name
+    name = file.name
     object_name = None
     fs = FileSystemStorage()
-    file_name = fs.save('static/'+ name, f)
+    file_name = fs.save('static/'+ name, file)
     name = name.split('/')[-1]
     uploaded_file_url = fs.url(name)
     print(uploaded_file_url, file_name)
